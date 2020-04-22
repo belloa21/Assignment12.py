287bf49c43e70901d55d50a84948aefe3e001e45
alexr <belloa@hartwick.edu>
2020-04-22 17:39:01 -0400

    Completed all menu choices and their code. I think, I think the if loop is correct. But they do work to check the string. Added comments.

--- assignment_12.py
+++ assignment_12.py
@@ -30,6 +30,7 @@ while menu != 11:
 
     if menu == 1:
         print("Time to check for a 'q'.")
+        # This will check the string to see if it includes a lower case q.
         find = re.findall(r'[q]', user_string)
         if find:
             print("True, which means your string does have a 'q' in it!")
@@ -38,6 +39,7 @@ while menu != 11:
 
     if menu == 2:
         print("Time to check if 'the' is in the string.")
+        # Reads the string for 'the' then captures it.
         find = re.findall(r'^(the)', user_string)
         if find:
             print("True, which means your string has 'the' in it!")
@@ -46,6 +48,7 @@ while menu != 11:
 
     if menu == 3:
         print("Time to check for a '*' in the string.")
+        # The upper case 'W' searches for a non word character, so it will search the string for the asteric.
         find = re.findall(r'\W[*]', user_string)
         if find:
             print("True, which means your string has a '*' in it.")
@@ -54,6 +57,7 @@ while menu != 11:
 
     if menu == 4:
         print("Time to check for any digits in your string.")
+        # Searches the string to find any digits.
         find = re.findall(r'\d', user_string)
         if find:
             print("True, which means your string has a digit in it!")
@@ -62,6 +66,7 @@ while menu != 11:
 
     if menu == 5:
         print("Time to check for a '.' in your string.")
+        # Searches for the period character.
         find = re.findall(r'\.', user_string)
         if find:
             print("True, which means your string has a '.' in it.")
@@ -70,6 +75,7 @@ while menu != 11:
 
     if menu == 6:
         print("Time to check if your string has two consecutive vowels in it.")
+        # Searches for the vowels twice in succession.
         find = re.findall(r'\w*[aeiou][aeiou]\w*', user_string)
         if find:
             print("True, which means your string has repeating vowels in it!")
@@ -78,6 +84,7 @@ while menu != 11:
 
     if menu == 7:
         print("Time to check if your string has any white space in it.")
+        # uses the RegEx to search for any white space in the string.
         find = re.findall(r'\s', user_string)
         if find:
             print("True, which means your string does have white space in it!")
@@ -86,6 +93,8 @@ while menu != 11:
 
     if menu == 8:
         print("Time to check if your string has any words with triple repeating letters in it.")
+        # I'll be honest I don't think I understand how this one works, but the \w searches for any character
+        # and having the {3} looks for a character that repeats three times. It works like how the repeated vowels worked.
         find = re.findall(r'\w{3}\w{3}', user_string)
         if find:
             print("True, which means that your string has a word that has three letters repeating in it!")
@@ -94,6 +103,8 @@ while menu != 11:
 
     if menu == 9:
         print("Time to check if your string starts with 'Hello' with an upper case H.")
+        # The hat RegEx matches lines that start with a certain word. In this case It'll search for Hello
+        # with the upper case h at the start of the string.
         find = re.findall(r'^Hello', user_string)
         if find:
             print("True, which means your string starts with Hello with an upper case 'H'!")
@@ -102,6 +113,8 @@ while menu != 11:
 
     if menu == 10:
         print("Time to check if your string has an email address in it.")
+        # Checks every character before the @, then the characters after that until the period,
+        # then all the characters after that.
         find = re.findall(r'\w+@\w+\.\w+', user_string)
         if find:
             print("True, which means your string contains an email address!")
